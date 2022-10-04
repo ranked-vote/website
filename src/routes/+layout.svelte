4c0d031157f42d04715f731b1f7a66356eb1174f
Felix Sargent <fsargent@users.noreply.github.com>
2022-10-03 17:31:39 -0700

    Fix styling paths.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,11 @@
 
+<script>
+	import { base } from '$app/paths';
+</script> 
+<svelte:head>
+	<link rel="stylesheet" href="{base}/style.css" />
+	<link rel="icon" type="image/png" href="{base}/icon.png" />
+</svelte:head>
 <main>
 	<slot></slot>
-</main>
+</main>	
