bb56659ef9f7decb0d538c72c70cfff222247dbe
Felix Sargent <fsargent@users.noreply.github.com>
2023-06-12 16:06:27 -0700

    Refactors file structure, moves components to a new lib folder, and updates imports. Also, adds a 404 error message to a page load function. These changes ensure better organization and maintenance.

--- src/routes/card/[...path]/+page.ts
+++ src/routes/card/[...path]/+page.ts
@@ -1,8 +1,15 @@
+import { error } from "@sveltejs/kit";
+
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
   const reports = import.meta.glob("$lib/reports/**/report.json", {
     eager: true,
   });
-  const report = reports[`$lib/reports/${params.path}/report.json`];
+  const report = reports[`/src/lib/reports/${params.path}/report.json`];
+  if (!report) {
+    throw error(404, {
+      message: "Not found",
+    });
+  }
   return { report: report, path: params.path };
 }
